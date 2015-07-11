917549171682f47daba32faa4e2367677e47a479
dmontiel242 <dmontiel242@hotmail.com>
2015-07-11 18:00:06 -0400

    output to png

--- plot1.R
+++ plot1.R
@@ -1,9 +1,11 @@
 setwd('.')
 
-file <- 'household_power_consumption.txt'
+file <- 'C:\\Users\\Dan\\Desktop\\Coursera\\EXData_Plotting1\\household_power_consumption.txt'
 data <- read.csv(file,sep=';',header = TRUE)
 #subset dates in February, input data is in d/m/y
 data2 <- data[data['Date']=='1/2/2007'|data['Date']=='2/2/2007',]
+png(filename = 'C:\\Users\\Dan\\Desktop\\Coursera\\EXData_Plotting1\\plot1.png', width = 480, height = 480, units = 'px')
 hist(as.numeric(as.character(data2$Global_active_power)),main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)', col = 'red')
 yticks = c(0,200,400,600,800,1000,1200)
 axis(2,at = yticks, labels = as.character(c(0,200,400,600,800,1000,1200)))
+dev.off()
